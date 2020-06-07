e92448cfdeab91dcbfd8fa572548d417af9d4501
OttoSouza <ottos.developer@gmail.com>
2020-06-07 09:55:57 -0300

    expenses

---
+++ src/controllers/ExpensesController.js
@@ -0,0 +1,63 @@
+const connection = require("../database/connection");
+
+module.exports = {
+  async index(request, response) {
+    try {
+      const expenses = await connection("expenses").select("*");
+      return response.status(200).json({ expenses });
+    } catch {
+      return response.status(400).json({ err: "There're no expenses!" });
+    }
+  },
+  async create(request, response) {
+    const { name, value } = request.body;
+    try {
+      const expenses = await connection("expenses").insert({
+        name,
+        value,
+      });
+      return response.status(204).json({ expenses });
+    } catch (error) {
+      return response.status(400).json({ err: "Put a name and value!" });
+    }
+  },
+
+  async update(request, response) {
+    const { name, value } = request.body;
+    const { id } = request.params;
+    try {
+      const expenses = await connection("expenses").where("id", id).update({
+        name,
+        value,
+      });
+      return response.status(204).json({ expenses });
+    } catch (error) {
+      return response.status(400).json({ err: "Expenses doesn't exist" });
+    }
+  },
+
+  async getById(request, response) {
+    const { id } = request.params;
+    try {
+      const expenses = await connection("expenses").where("id", id).first();
+      if (!expenses)
+        return response
+          .status(400)
+          .json({ message: "Expenses doesn't exists" });
+
+      return response.status(200).json(expenses);
+    } catch {
+      return response.status(400).json({ err: "Expenses doesn't exist" });
+    }
+  },
+
+  async delete(request, response) {
+    const { id } = request.params;
+    try {
+      await connection("expenses").delete().where({ id: id });
+      return response.status(200).json({ message: "Success deleted" });
+    } catch {
+      return response.status(400).json({ err: "Expenses doesn't exist" });
+    }
+  },
+};
--- src/routes.js
+++ src/routes.js
@@ -1,11 +1,23 @@
 const express = require('express');
 const routes = express.Router();
 const incomesController = require('./controllers/IncomesController');
+const expensesController = require('./controllers/ExpensesController');
 
-routes.get('/list', incomesController.index)
-routes.get('/list/:id', incomesController.getById)
-routes.post('/create', incomesController.create)
-routes.put('/update/:id', incomesController.update)
-routes.delete('/delete/:id', incomesController.delete)
+routes.get('/', (require, response) => {
+  return response.send('welcome finanses app');
+});
+
+routes.get('/list-incomes', incomesController.index)
+routes.get('/list-incomes/:id', incomesController.getById)
+routes.post('/create-incomes', incomesController.create)
+routes.put('/update-incomes/:id', incomesController.update)
+routes.delete('/delete-incomes/:id', incomesController.delete)
+
+
+routes.get('/list-expenses', expensesController.index)
+routes.get('/list-expenses/:id', expensesController.getById)
+routes.post('/create-expenses', expensesController.create)
+routes.put('/update-expenses/:id', expensesController.update)
+routes.delete('/delete-expenses/:id', expensesController.delete)
 
 module.exports = routes;
