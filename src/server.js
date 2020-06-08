942c20280369db5f51ce35ee448c6503ad70db37
OttoSouza <ottos.developer@gmail.com>
2020-06-08 20:38:21 -0300

    cors againn

--- src/server.js
+++ src/server.js
@@ -6,7 +6,7 @@ const app = express();
 app.use(express.json());
 
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Origin", "http://localhost:3000/");
   res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
   app.use(cors());
   next();
