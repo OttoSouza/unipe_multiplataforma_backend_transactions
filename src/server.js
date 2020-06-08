2e57e384cec4406aaf8a2a4bc3f907e5201a84ba
OttoSouza <ottos.developer@gmail.com>
2020-06-08 20:05:27 -0300

    cors again

--- src/server.js
+++ src/server.js
@@ -4,13 +4,12 @@ const routes = require("./routes");
 const app = express();
 app.use(express.json());
 
-app.use(cors());
-// app.use((require, response, next) => {
-//   response.header("Access-Control-Allow-Origin", "*");
-//   response.header("Access-Control-Allow-Origin", "GET, PUT, POST, DELETE");
-//   app.use(cors());
-//   next();
-// });
+app.use((require, response, next) => {
+  response.header("Access-Control-Allow-Origin", "http://localhost:3000/");
+  response.header("Access-Control-Allow-Origin", "GET, PUT, POST, DELETE");
+  app.use(cors());
+  next();
+});
 
 app.use(routes);
 
