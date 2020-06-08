fa635bfd663bd812b6595d5de09b2113c49fbf8c
OttoSouza <ottos.developer@gmail.com>
2020-06-08 19:57:29 -0300

    cors again

--- src/server.js
+++ src/server.js
@@ -2,12 +2,14 @@ const express = require("express");
 const cors = require("cors");
 const routes = require("./routes");
 const app = express();
-app.use(
-  cors({
-    origin: ["https://finances-back.herokuapp.com/", "http://localhost:3000"],
-  })
-);
 app.use(express.json());
+
+  app.use((require, response, next) => {
+    response.header("Access-Control-Allow-Origin", "*")
+    app.use(cors());
+    next();
+  });
+
 app.use(routes);
 
 app.listen(process.env.PORT || 5000);
