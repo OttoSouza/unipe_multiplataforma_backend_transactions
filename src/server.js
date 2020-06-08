4a7b4b0b08fc7988f625dd290521e29677a4b116
OttoSouza <ottos.developer@gmail.com>
2020-06-08 19:50:32 -0300

    cors

--- src/server.js
+++ src/server.js
@@ -1,10 +1,13 @@
 const express = require("express");
 const cors = require("cors");
-
-const routes = require('./routes')
+const routes = require("./routes");
 const app = express();
-app.use(cors());
+app.use(
+  cors({
+    origin: ["https://finances-back.herokuapp.com/", "http://localhost:3000"],
+  })
+);
 app.use(express.json());
-app.use(routes)
+app.use(routes);
 
 app.listen(process.env.PORT || 5000);
