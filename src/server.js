2d0ae6d7d9e1fa6d9457ddb4606139810f8a9907
OttoSouza <ottos.developer@gmail.com>
2020-06-08 20:02:40 -0300

    cors again

--- src/server.js
+++ src/server.js
@@ -4,11 +4,13 @@ const routes = require("./routes");
 const app = express();
 app.use(express.json());
 
-  app.use((require, response, next) => {
-    response.header("Access-Control-Allow-Origin", "*")
-    app.use(cors());
-    next();
-  });
+app.use(cors());
+// app.use((require, response, next) => {
+//   response.header("Access-Control-Allow-Origin", "*");
+//   response.header("Access-Control-Allow-Origin", "GET, PUT, POST, DELETE");
+//   app.use(cors());
+//   next();
+// });
 
 app.use(routes);
 
