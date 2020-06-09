7789e90173ab62cbe8c0854f182fa403c75fcee2
OttoSouza <ottos.developer@gmail.com>
2020-06-09 19:00:51 -0300

    fixing something

--- src/database/migrations/20200607084312_expenses.js
+++ src/database/migrations/20200607084312_expenses.js
@@ -2,7 +2,7 @@ exports.up = function (knex) {
   return knex.schema.createTable("expenses", (table) => {
     table.increments("id").primary();
     table.string("name").notNullable();
-    table.decimal("value").notNullable();
+    table.bigInteger("value").notNullable();
   });
 };
 
